8775586a570613a517df8074b46e56f252787359
Dlasyd <malevanyy@gmail.com>
2015-12-08 22:46:11 +0300

    addAdjacentTile rewritten

--- src/carcassonne/model/TileDirections.java
+++ src/carcassonne/model/TileDirections.java
@@ -1,5 +1,9 @@
 package carcassonne.model;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Andrey on 03/12/15.
  */
@@ -40,5 +44,32 @@ public enum TileDirections {
         }
         assert (false);
         return CENTER;
+    }
+
+    public Set<TileDirections> getEdge() {
+        Set<TileDirections> result = new HashSet<>();
+        switch (this) {
+            case NORTH:
+            case NNW:
+            case NNE:
+                result.addAll(Arrays.asList(NORTH, NNW, NNE));
+                break;
+            case SOUTH:
+            case SSW:
+            case SSE:
+                result.addAll(Arrays.asList(SOUTH, SSW, SSE));
+                break;
+            case EAST:
+            case EEN:
+            case EES:
+                result.addAll(Arrays.asList(EAST, EEN, EES));
+                break;
+            case WEST:
+            case WWN:
+            case WWS:
+                result.addAll(Arrays.asList(WEST, WWN, WWS));
+                break;
+        }
+        return result;
     }
 }
---
+++ src/carcassonne/model/TileDirectionsTest.java
@@ -0,0 +1,28 @@
+package carcassonne.model;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import static carcassonne.model.TileDirections.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is a part of Carcassonne project.
+ * The project is created for learning and practicing java
+ * and not intended for distribution.
+ * Created by Andrey on 08/12/15.
+ */
+public class TileDirectionsTest {
+    public TileDirections direction;
+
+    @Test
+    public void getEdgeSet() {
+        Set<TileDirections> expected = new HashSet<>();
+        direction = EAST;
+        expected.addAll(Arrays.asList(EAST, EEN, EES));
+        assertEquals("TileDirection should return Set of TileDirections of edges", expected, direction.getEdge());
+    }
+}
